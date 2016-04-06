38ab173545c89d858fa91a5749d2c199c4e7fa2e
elianenassif <nassif.eliane@gmail.com>
2016-04-06 12:51:11 +0300

    added samples

---
+++ sample1/config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+    serviceName: "sampleFailing",
+    serviceGroup: "test",
+    serviceVersion: 1,
+    requestTimeout: 30,
+    requestTimeoutRenewal: 5,
+    extKeyRequired: true,
+    type: 'service',
+    prerequisites: {
+        cpu: '',
+        memory: ''
+    },
+	"awareness": true,
+    "errors": {
+        "600": "Database error",
+        "601": "Missing info"
+    },
+    "schema": {
+
+        "/getInfo": {
+            "_apiInfo": {
+                "l": "Get Info",
+                "group": "Information",
+                "groupMain": true
+            },
+            "email": {
+                "source": ['query.email'],
+                "required": false,
+                "validation": {
+                    "type": "string",
+                    "format": "email"
+                }
+            }
+        }
+    }
+};
