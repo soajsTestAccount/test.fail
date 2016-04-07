9c00e8d4d0f917213b815341a1ca8952262a5317
elianenassif <nassif.eliane@gmail.com>
2016-04-07 11:38:38 +0300

    updates

---
+++ sample4/config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+	serviceName: "sampleDaemonFail2",
+	serviceGroup: "test",
+	servicePort: 2004,
+	type: 'daemonss',
+	prerequisites: {},
+	serviceVersion: 1,
+	errors: {},
+	"schema": {
+		"testJob": {
+			"l": "test Job"
+		}
+	}
+};
